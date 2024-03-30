8d2c3b4b2a54751ef5ebb28618fd21d71cd1e4c0
Savyo <65571505+Jose-Savyo@users.noreply.github.com>
2024-03-30 09:50:51 -0300

    Add files via upload
    Showing the results.

--- TESTE_MEDIAPIPE_V1.py
+++ TESTE_MEDIAPIPE_V1.py
@@ -4,6 +4,8 @@ import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from visualization_utilities import *
+from draw_landmarks_on_image import draw_landmarks_on_image
 
 # MODELO GESTURRE RECOGNIZER
 model_path = 'D:/IA_MOUSE/gesture_recognizer.task'
@@ -14,7 +16,9 @@ options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
 
 # ENTRADA DE IMAGEM
-img = mp.Image.create_from_file('images/img1.jpg')                          
+img = mp.Image.create_from_file('D:/IA_MOUSE/ShootingGameAI-main/images/img1.jpg')
 
 gesture_recognition_result = recognizer.recognize(img)
 
+annotated_image = draw_landmarks_on_image(img.numpy_view(), gesture_recognition_result)
+cv2.imshow('test', cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
